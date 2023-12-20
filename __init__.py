3171c1377fea2ce847af85c2ba6e2c283ce5bf1d
Indy Ray <arelius@gmail.com>
2023-12-19 18:56:52 -0800

    Launching model info process in init.

--- __init__.py
+++ __init__.py
@@ -4,15 +4,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import server
+import asyncio
 
 async def handleNode(request):
-    print(request)
-    print(request.url)
-    print(request.rel_url)
-    #return aiohttp.web.json_response([1, 2])
-    print(request.match_info['tail'])
     url = "http://127.0.0.1:8189/{}".format(request.match_info['tail'])
-    print(url)
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers = request.headers) as resp:
             return aiohttp.web.Response(
@@ -21,7 +16,16 @@ async def handleNode(request):
                 headers = resp.headers
             )
 
+async def launchNode():
+    serverjs = os.path.abspath(os.path.join(os.path.dirname(__file__), 'host/src/server.js'))
+    proc = await asyncio.create_subprocess_exec(
+        'node', serverjs, '-d', '-p', '8189',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    #await proc.wait()
+
 def init():
+    asyncio.run(launchNode())
     server.PromptServer.instance.app.add_routes([
         aiohttp.web.route('*', '/model_info/{tail:.*}', handleNode)
     ])
